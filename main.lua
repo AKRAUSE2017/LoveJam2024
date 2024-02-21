23089ae5aa24d6cf0e29da1f6ec1f0480956779d
Alena <akrause2017@fau.edu>
2024-02-20 21:18:05 -0500

    dialog impl

--- main.lua
+++ main.lua
@@ -3,11 +3,13 @@ Class = require('imports.class')
 
 require('helpers.constants')
 require('helpers.utils')
+require('helpers.dialog_list')
 
 require('classes.icon')
 require('classes.window')
 require('classes.button')
 require('classes.text_box')
+require('classes.dialog')
 
 function love.load() -- happens when the game starts
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -21,41 +23,42 @@ function love.load() -- happens when the game starts
 
     last_mouse_click = {time=0, x=0, y=0} -- time, x, y
 
+    dialog = Dialog(VIRTUAL_WIDTH/2 - 1100/2, 555, 1100, 100)
+
     current_day = 1
     applications = {}
 
-    text = ""
+    background = love.graphics.newImage("assets/background.jpg")
 
     -- application for folder and file explorer
-    -- applications["folder"] = {
-    --     icon=Icon(10, 10, FOLDER_ICON_W, FOLDER_ICON_H, "Folder", love.graphics.newImage("assets/file_explorer/folder.png")),
-    --     window=Window(50, 50, VIRTUAL_WIDTH-100, VIRTUAL_HEIGHT-100, "Shared Folder", love.graphics.newImage("assets/file_explorer/file_explorer.png"), 
-    --         {love.graphics.newImage("assets/file_explorer/file_explorer_screen.png")}, -- list of screens
-    --         {Button(75, 75, 50, 20, "folder_btn_1", love.graphics.newImage("assets/generic_button.png"))}, -- list of buttons
+    applications["folder"] = {
+        icon=Icon(10, 10, FOLDER_ICON_W, FOLDER_ICON_H, "Folder", love.graphics.newImage("assets/file_explorer/folder.png")),
+        window=Window(50, 50, VIRTUAL_WIDTH-100, VIRTUAL_HEIGHT-100, "Shared Folder", love.graphics.newImage("assets/file_explorer/file_explorer.png"), 
+            {love.graphics.newImage("assets/file_explorer/file_explorer_screen.png")}, -- list of screens
+
+            {Button(1190, 55, 20, 20, "file_explorer_close")}, -- list of buttons
             
-    --         {} -- list of text boxes
-    --     )
-    -- }
+            {} -- list of text boxes
+        )
+    }
 
-    -- -- application for email and email window(?)
-    -- applications["email"] = {
-    --     icon=Icon(10, 80, FOLDER_ICON_W, FOLDER_ICON_H, "Email", love.graphics.newImage("assets/email/email.png")),
-    --     window=Window(50, 50, VIRTUAL_WIDTH-100, VIRTUAL_HEIGHT-100, "Email", love.graphics.newImage("assets/email/email_window.png"), 
-    --         {love.graphics.newImage("assets/email/email_clicked_screen.png"), -- list of screens
-    --          love.graphics.newImage("assets/email/email_clicked_screen2.png")}, 
+    -- application for email and email window(?)
+    applications["email"] = {
+        icon=Icon(10, 70, EMAIL_ICON_W, EMAIL_ICON_H, "Email", love.graphics.newImage("assets/email/email.png")),
+        window=Window(50, 50, VIRTUAL_WIDTH-100, VIRTUAL_HEIGHT-100, "Email", love.graphics.newImage("assets/email/email_window.png"), 
+            {}, 
 
-    --         {Button(75, 75, 50, 20, "email_button_inbox", love.graphics.newImage("assets/generic_button.png")), -- list of buttons
-    --          Button(150, 75, 50, 20, "email_button_outbox", love.graphics.newImage("assets/generic_button.png")),
-    --          Button(75, 150, 50, 20, "message 1 button"),
-    --          Button(75, 300, 50, 20, "message 2 button")} ,
+            {Button(70, 95, 92, 20, "email_button_inbox"), -- list of buttons
+             Button(198, 95, 92, 20, "email_button_outbox"),
+             Button(1190, 55, 20, 20, "email_close")} ,
 
-    --         {} -- list of text boxes
-    --     )
-    -- } 
+            {} -- list of text boxes
+        )
+    } 
 
     -- -- application for web browser (name pending) application
     applications["web"] = {
-       icon=Icon(10, 150, 37, 37, "Web", love.graphics.newImage("assets/web/web_icon.png")),
+       icon=Icon(10, 160, WEB_ICON_W, WEB_ICON_H, "Web", love.graphics.newImage("assets/web/web_icon.png")),
        window=Window(50, 50, VIRTUAL_WIDTH-100, VIRTUAL_HEIGHT-100, "Web", love.graphics.newImage("assets/web/web_window.png"),
            {love.graphics.newImage("assets/web/web_screen.png"),
             love.graphics.newImage("assets/web/web_screen_dark.png"),
@@ -64,13 +67,15 @@ function love.load() -- happens when the game starts
            {Button(1150, 90, 62, 19, "web_light_mode", love.graphics.newImage("assets/web/web_button_light_mode.png")),
             Button(1150, 90, 62, 19, "web_dark_mode", love.graphics.newImage("assets/web/web_button_dark_mode.png")),
             Button(1190, 55, 20, 20, "web_close"),
-            Button(1190, 55, 20, 20, "web_bookmark")}, -- list of buttons
+            Button(1004, 88, 14, 20, "web_bookmark")}, -- list of buttons
 
            {TextBox(90, 88, 890, 20, "editable"), -- list of text boxes
             TextBox(1024, 91, 0, 0, "static", "Data Portal"),
             TextBox(67, 62, 0, 0, "static", "Enterprise Safe Browser")} 
        )
-    } 
+    }
+
+    game_state = "dialog"
 end
 
 function love.resize(w,h)
@@ -90,8 +95,16 @@ function love.textinput(t)
 end
 
 function love.keypressed(key)
-    print(key)
-    if key == "backspace" or key == "return" then
+    if dialog.active then
+        if dialog.line_index == #dialog_prompts[dialog.prompt_index] then
+            dialog.line_index = 1
+            dialog.prompt_index = dialog.prompt_index + 1
+            dialog.active = false
+            game_state = "play"
+        else
+            dialog.line_index = dialog.line_index + 1
+        end
+    elseif key == "backspace" or key == "return" then
         for application_name, application in pairs(applications) do
             for _, text_box in pairs(application.window.text_boxes) do
                 if text_box.active and key == "backspace" then
@@ -106,6 +119,8 @@ end
 
 function love.draw() -- render objects on screen
     push:start()
+
+    love.graphics.draw(background, 0, 0, 0, 0.75)
     
     for _, application in pairs(applications) do
         application.icon:render()
@@ -113,6 +128,8 @@ function love.draw() -- render objects on screen
     for _, application in pairs(applications) do
         application.window:render()
     end
+
+    dialog:render()
 
     push:finish()
 end
@@ -156,19 +173,24 @@ function handle_mouse_click_window(mouse)
 end
 
 function love.mousepressed() --  double click
-    local mouse = utils_get_mouse_data(scale_x, scale_y)
-    print(mouse.x, mouse.y)
-	
-    handle_mouse_click_icon(mouse)
-    handle_mouse_click_window(mouse)
+    if game_state == "play" then
+        local mouse = utils_get_mouse_data(scale_x, scale_y)
+        print(mouse.x, mouse.y)
+        
+        handle_mouse_click_icon(mouse)
+        handle_mouse_click_window(mouse)
+    end
 end
 
 function love.update() -- runs every frame
+    if dialog.active then game_state = "dialog" end
+
     local mouse = utils_get_mouse_data(scale_x, scale_y) -- returns {button_1, button_2, x, y, w=1, h=1}
 
     for _, application in pairs(applications) do
         application.icon:set_state(mouse, last_mouse_click)
         application.icon:update(mouse)
     end
-    
+
+    dialog:check_and_set_active(current_day, applications)
 end
